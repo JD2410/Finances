736c9459c927b4def4853cfa5a468685517e2b0b
Jonathan Delves <jonathandelves@hotmail.com>
2026-09-10 08:45:07 +0100

    Humaniser for currency - added total value to sidebar

--- finances/tags/templatetags/nav_accounts.py
+++ finances/tags/templatetags/nav_accounts.py
@@ -1,9 +1,33 @@
 from django import template
-from accSavings.models import Accounts
+from accSavings.models import Accounts, Transactions
+from django.db.models import Sum
+
 
 register = template.Library()
 
 @register.inclusion_tag('_partials/accounts.html')
 def render_accounts():
-    records = Accounts.objects.all()
-    return {'records': records}
+    accounts = Accounts.objects.all()
+    accountDetails = []
+    
+    for account in accounts:
+
+        total = 0.00
+        if account.accountType.accumulate:
+            get_transactions_total = Transactions.objects.values('accountId').annotate(total=Sum('amount')).filter(accountId = account.id)
+            if len(get_transactions_total):
+                total = get_transactions_total[0]['total']
+        else:
+            get_transactions_total = Transactions.objects.values('amount').filter(accountId = account.id).order_by('-date').first()
+            if len(get_transactions_total):
+                total = get_transactions_total['amount']
+
+        accountDetails.append({
+            'name': account.name,
+            'total': total
+        })
+
+        print(accountDetails)
+        
+
+    return {'records': accountDetails}
