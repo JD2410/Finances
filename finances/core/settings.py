dfcafbcf070dc741d9158d9d389a7d31886b3d95
Jonathan Delves <jonathandelves@hotmail.com>
2026-09-18 10:16:25 +0100

    Updated project folders name finances to core

--- finances/core/settings.py
+++ finances/core/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'finances.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'finances.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
