b875ccd601ac0f9786d2d7b60dfcc41f0756e5d6
Jonathan Delves <jonathandelves@hotmail.com>
2026-09-18 10:10:59 +0100

    changed assets location names - aded migration folder for the dashboard - changed root template to _partials

--- finances/finances/settings.py
+++ finances/finances/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'finances.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / '_partials'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
